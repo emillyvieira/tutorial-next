4bbd5701804e6245157424c657a92e232007754e
emillyvieira <emilly.vieira190505@gmail.com>
2025-02-24 21:26:56 -0300

    hooks

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
+'use client'
 import React from 'react';
 import Header from '@/components/Header';
 import Sidebar from '@/components/Sidebar';
 import MainContent from '@/components/MainContent';
 import Footer from '@/components/Footer';
+import Link from 'next/link';
+import { useState } from 'react';
 
 function App() {
+  const [contador, setContador] =  useState(3)
+
+  console.log("Carregando...")
+  
   return (
     <div className="App flex flex-col min-h-screen bg-off-white text-purple-800">
       <Header />
@@ -14,9 +21,17 @@ function App() {
         </div>
         <div className="w-3/4">
           <MainContent />
+          <h2>Contador</h2>
+          <p>{contador}</p>
+          <button className='border p-2 ' onClick={() => {
+            setContador(contador + 1 ) 
+            console.log(contador)
+          }}>+1</button>
+
         </div>
       </div>
       <Footer />
+
     </div>
   );
 }
